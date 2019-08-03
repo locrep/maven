765e83882914023f0f1bc32a82375051e62a074a
Murat Ungor <muratungor@gmail.com>
2019-08-03 17:17:42 +0300

    database_handler deleted

--- maven/db_handler.go
+++ maven/db_handler.go
@@ -1,1 +1,17 @@
 package maven
+
+
+/*
+ * Database handler
+ *
+ * This will be changed and updated drastically through out the development stage
+ *
+ * func saveArtifactRecord(artif *Artifact) bool
+ *   This function should save the given artifact to database.
+ *
+ * func deleteArtifactRecord(artif *Artifact) bool
+ *   This function should delete the given artifact from database.
+ *
+ * func checkArtifactRecord(artif *Artifact) bool
+ *   This function should check if the given artifact exists on the database.
+ */
