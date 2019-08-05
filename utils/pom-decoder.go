67b2de6c65752a496acf81a7540ffa86d1cd7202
alpay bilgiç <bilgicalpay@gmail.com>
2019-08-05 23:15:25 +0300

    organization commented

--- utils/pom-decoder.go
+++ utils/pom-decoder.go
@@ -10,7 +10,7 @@ type Project struct {
 
     XMLName xml.Name `xml:"project"`
     Project[] Licenses `xml:"licenses"`
-    Project[] Organization `xml:"organization"`
+        //	Project  		[]Organization  `xml:"organization"`
     ModelVersion string `xml:"modelVersion"`
     GroupId string `xml:"groupId"`
     ArtifactId string `xml:"artifactId"`
@@ -18,61 +18,72 @@ type Project struct {
     Name string `xml:"name"`
     Description string `xml:"description"`
     Url string `xml:"url"`
+}
 
-    type Licenses struct {
 
-        XMLName xml.Name `xml:"licenses"`
-        License License `xml:"license"`
+type Licenses struct {
+
+    XMLName xml.Name `xml:"licenses"`
+    License License `xml:"license"`
+
+}
+
+
+type License struct {
+
+    XMLName xml.Name `xml:"license"`
+    Name string `xml:"name"`
+    Url string `xml:"url"`
+    Distribution string `xml:"distribution"`
+}
+
+
+//type Organization struct {
+//
+//	XMLName 		xml.Name	`xml:"organization"`
+//	Name   			string		`xml:"name"`
+//	Url   			string		`xml:"url"`
+//
+//						}
+
+
+
+
+func main() {
+
+    // Open our xmlFile
+    xmlFile, err: = os.Open("sample.pom")
+        // if we os.Open returns an error then handle it
+    if err != nil {
+        fmt.Println(err)
     }
 
-    type License struct {
+    fmt.Println("Successfully Opened pom")
+        // defer the closing of our xmlFile so that we can parse it later on
+    defer xmlFile.Close()
 
-        XMLName xml.Name `xml:"license"`
-        Name string `xml:"name"`
-        Url string `xml:"url"`
-        Distribution string `xml:"distribution"`
+    // read our opened xmlFile as a byte array.
+    byteValue, _: = ioutil.ReadAll(xmlFile)
+
+    // we initialize our Users array
+
+
+    var project Project
+
+    xml.Unmarshal(byteValue, & project)
+
+    for i: = 0;
+    i < len(project.Project);
+    i++{
+
+        fmt.Println("project Name " + project.Name)
+        fmt.Println("project groupId =" + project.GroupId)
+        fmt.Println("project artifactId = " + project.ArtifactId)
+        fmt.Println("project Version =" + project.Version)
+            //	fmt.Println("project organization =" + project.Project[i].Organization.Name)
+        fmt.Println("license " + project.Project[i].License.Name)
+        fmt.Println("project description =" + project.Description)
+        fmt.Println("project url =" + project.Url)
     }
 
-    type Organization struct {
-
-        XMLName xml.Name `xml:"organization"`
-        Name string `xml:"name"`
-        Url string `xml:"url"`
-    }
-
-    func main() {
-
-        // Open our xmlFile
-        xmlFile, err: = os.Open("sample.pom")
-            // if we os.Open returns an error then handle it
-        if err != nil {
-            fmt.Println(err)
-        }
-
-        fmt.Println("Successfully Opened pom")
-            // defer the closing of our xmlFile so that we can parse it later on
-        defer xmlFile.Close()
-
-        // read our opened xmlFile as a byte array.
-        byteValue, _: = ioutil.ReadAll(xmlFile)
-
-        // we initialize our Users array
-
-        var project Project
-
-        xml.Unmarshal(byteValue, & project)
-
-        for i: = 0;
-        i < len(project.Project);
-        i++{
-            fmt.Println("project Name " + project.Name)
-            fmt.Println("project groupId =" + project.GroupId)
-            fmt.Println("project artifactId = " + project.ArtifactId)
-            fmt.Println("project Version =" + project.Version)
-            fmt.Println("project organization =" + project.Project[i].Organization.Name)
-            fmt.Println("license " + project.Project[i].License.Name)
-            fmt.Println("project description =" + project.Description)
-            fmt.Println("project url =" + project.Url)
-        }
-
-    }
+}
