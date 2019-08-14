313f1dcba18109e5f697cf1a3838ff19ad47bffe
berkayoz <berkaytknoz@gmail.com>
2019-08-14 10:22:59 +0300

    Database functions described.

--- maven/db_handler.go
+++ maven/db_handler.go
@@ -9,6 +9,39 @@ import (
 
 var pool *sql.DB // Database connection pool.
 
+func insertArtifactFile(artRowId int, filename string, checksum string) bool {
+	/*
+	 * We should keep our groupID, artifactID and version in a seperate table instead of writing them multiple times for each file.
+	 * And then we can associate the artifacts table and files table by the artifacts row id(artRowId) eg. 12
+	 * If any error occurs the return should be false.
+	 */
+}
+
+func artifactSaved(groupID string, artifactID string, version string) (bool, int) {
+	/*
+	 * This function should check if there is a row that contains groupID, artifactID and version exists.
+	 * If exists it should return true, nil otherwise it should return false, row id(artRowId)
+	 */
+}
+
+func insertArtifact(groupID string, artifactID string, version string) (bool, int) {
+	/*
+	 * This function should insert  groupID, artifactID, version into "artifacts" table. After the insert it should return the row id(artRowId)
+	 * It should check if the artifact is already saved with artifactSaved(). If saved it should return the row id returned by artifactSaved()
+	 * If any error occurs the first return should be false.
+	 */
+}
+
+func insertFile(groupID string, artifactID string, version string, filename string) {
+	/*
+	 * This is the main handler this function should insert the artifact if not exists and then insert the file to files table.
+	 * Each required function is explained above.
+	 * Function should proceed like:
+	 * success, artRowId := insertArtifact(...)
+	 * fsuccess := insertArtifactFile(artRowId, ...)
+	 */
+}
+
 func connect() {
 	// Open up our database connection.
 	pool, err := sql.Open("mysql", "root:******@tcp(172.17.0.2:3306)/locrep")
