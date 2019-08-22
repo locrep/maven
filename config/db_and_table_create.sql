9d09c331307eed2e86c4680506c8da93987beec5
Murat Ungor <muratungor@gmail.com>
2019-08-22 13:01:36 +0300

    doesnt matter

--- config/db_and_table_create.sql
+++ config/db_and_table_create.sql
@@ -1,15 +1,22 @@
 DROP DATABASE IF EXISTS locrep;
 CREATE DATABASE locrep;
 use locrep;
-CREATE TABLE maven (
-    id INT NOT NULL AUTO_INCREMENT,
-    filename TINYTEXT NOT NULL,
+CREATE TABLE maven_artifacts (
+    artRowId INT NOT NULL AUTO_INCREMENT,
+    //filename TINYTEXT NOT NULL,
     version VARCHAR(10),
-    md5Hash VARCHAR(32),
-    sha1Hash VARCHAR(40),
-    groupid TINYTEXT,
-    artifactid TINYTEXT,
+    //md5Hash VARCHAR(32),
+    //sha1Hash VARCHAR(40),
+    groupID TINYTEXT,
+    artifactID TINYTEXT,
     dateTime DATETIME DEFAULT CURRENT_TIMESTAMP,
-    primary key (id)
+    primary key (artRowId),
+    uniq (groupid,artifactid,version)
     
+);
+
+CREATE TABLE maven_artifact_file (
+    artRowId INT NOT NULL,
+    filename TINYTEXT NOT NULL,
+    checksum VARCHAR(40)
 );
