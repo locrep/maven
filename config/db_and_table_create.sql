5d409f884f625be3b9ec846012134d110ef17067
Murat Ungor <muratungor@gmail.com>
2019-09-19 22:20:17 +0300

    db handler and table create.sql

--- config/db_and_table_create.sql
+++ config/db_and_table_create.sql
@@ -3,12 +3,9 @@ CREATE DATABASE locrep;
 use locrep;
 CREATE TABLE maven_artifacts (
     artRowId INT NOT NULL AUTO_INCREMENT,
-    //filename TINYTEXT NOT NULL,
-    version VARCHAR(10),
-    //md5Hash VARCHAR(32),
-    //sha1Hash VARCHAR(40),
     groupID TINYTEXT,
     artifactID TINYTEXT,
+    version VARCHAR(10),
     dateTime DATETIME DEFAULT CURRENT_TIMESTAMP,
     primary key (artRowId),
     uniq (groupid,artifactid,version)
@@ -16,6 +13,7 @@ CREATE TABLE maven_artifacts (
 );
 
 CREATE TABLE maven_artifact_file (
+    id AUTO_INCREMENT
     artRowId INT NOT NULL,
     filename TINYTEXT NOT NULL,
     checksum VARCHAR(40)
--- maven/db_handler.go
+++ maven/db_handler.go
@@ -9,45 +9,83 @@ import (
 
 var pool *sql.DB // Database connection pool.
 
-// func insertArtifactFile(artRowId int, filename string, checksum string) bool {
-// 	/*
-// 	 * We should keep our groupID, artifactID and version in a seperate table instead of writing them multiple times for each file.
-// 	 * And then we can associate the artifacts table and files table by the artifacts row id(artRowId) eg. 12
-// 	 * If any error occurs the return should be false.
-// 	 */
-// }
+func insertArtifactFile(artRowID int64, filename string, checksum string) bool {
+	/*
+	 * We should keep our groupID, artifactID and version in a seperate table instead of writing them multiple times for each file.
+	 * And then we can associate the artifacts table and files table by the artifacts row id(artRowId) eg. 12
+	 * If any error occurs the return should be false.
+	 */
+}
 
-// func artifactSaved(groupID string, artifactID string, version string) (bool, int) {
-// 	/*
-// 	 * This function should check if there is a row that contains groupID, artifactID and version exists.
-// 	 * If exists it should return true, nil otherwise it should return false, row id(artRowId)
-// 	 */
-// }
+func artifactSaved(groupID string, artifactID string, version string) (bool, int) {
+	sqlStatement := "select artRowId from maven_artifacts where version= $1 and groupID = $2 and artifactID = $3 "
+	var id int
+	row := pool.QueryRow(sqlStatement, version, groupID, artifactID)
+	switch err := row.Scan(&id); err {
+	case sql.ErrNoRows:
+		return false, 0
+	case nil:
+		return true, id
+	default:
+		panic(err)
+	}
+	/*
+	 * This function should check if there is a row that contains groupID, artifactID and version exists.
+	 * If exists it should return true, nil otherwise it should return false, row id(artRowId)
+	 */
+}
 
-// func insertArtifact(groupID string, artifactID string, version string) (bool, int) {
-// 	/*
-// 	 * This function should insert  groupID, artifactID, version into "artifacts" table. After the insert it should return the row id(artRowId)
-// 	 * It should check if the artifact is already saved with artifactSaved(). If saved it should return the row id returned by artifactSaved()
-// 	 * If any error occurs the first return should be false.
-// 	 */
-// 	sqlStatement := "insert into maven_artifacts (version,groupID,artifactID) VALUES ($1,$2,$3)"
-// 	res, _ := pool.Query(sqlStatement, version, groupID, artifactID)
-// }
+func insertArtifact(groupID string, artifactID string, version string) (bool, int64) {
+	/*
+	 * This function should insert  groupID, artifactID, version into "maven_artifacts" table. After the insert it should return the row id(artRowId)
+	 * It should check if the artifact is already saved with artifactSaved(). If saved it should return the row id returned by artifactSaved()
+	 * If any error occurs the first return should be false.
+	 */
 
-// func insertFile(groupID string, artifactID string, version string, filename string) {
-// 	/*
-// 	 * This is the main handler this function should insert the artifact if not exists and then insert the file to files table.
-// 	 * Each required function is explained above.
-// 	 * Function should proceed like:
-// 	 * success, artRowId := insertArtifact(...)
-// 	 * fsuccess := insertArtifactFile(artRowId, ...)
-// 	 */
-// 	connect()
+	sqlStatement := "insert into maven_artifacts (version,groupID,artifactID) VALUES ($1,$2,$3)"
+	res, err := pool.Exec(sqlStatement, version, groupID, artifactID)
+	if err != nil {
+		succes, artRowId := artifactSaved(groupID, artifactID, version)
+		return false, 0
+	} else {
+		id, err := res.LastInsertId()
+		return true, id
+	}
+}
 
-// 	succes, artRowId := insertArtifact(groupID, artifactID, version)
-// 	res, _ := pool.Query("insert into maven_artifacts ")
+func insertFile(groupID string, artifactID string, version string, filename string) bool {
+	/*
+	 * This is the main handler this function should insert the artifact if not exists and then insert the file to files table.
+	 * Each required function is explained above.
+	 * Function should proceed like:
+	 * success, artRowId := insertArtifact(...)
+	 * fsuccess := insertArtifactFile(artRowId, ...)
+	 */
+	connect()
+	var artRowId int
+	var isArtifactSaved bool
+	isArtifactSaved, artRowId = artifactSaved(groupID, artifactID, version)
+	if !isArtifactSaved {
+		var success bool
+		success, artRowId = insertArtifact(groupID, artifactID, version)
+		if !success {
+			return false
+		}
+	}
 
-// }
+	var isFileSaved bool
+
+	isFileSaved = isFileSaved(fileName, artRowId)
+	if !isFileSaved {
+		isFileSaved = insertArtifactFile(artRowId, filename, checksum)
+		if !isFileSaved {
+			return false
+		}
+	}
+
+	return true
+
+}
 
 func connect() {
 	// Open up our database connection.
